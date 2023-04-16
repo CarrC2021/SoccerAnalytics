9fc1fb5d299bc953bb389d8024e27454f2a8aa06
CarrC2021 <ccarr2017@gmail.com>
2023-04-16 19:55:13 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Soccer Analytics Final Project',
+    version='0.1.0',
+    description='Creating basic models used in modern football/soccer analytics',
+    long_description=readme,
+    author='Casey Carr',
+    author_email='ccarr2017@gmail.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
